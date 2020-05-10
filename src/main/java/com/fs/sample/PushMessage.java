abc64b531ecce167852ad38f403afbfb7aff127b
Yogesh <yogesh.lakhole@gmail.com>
2020-05-10 21:29:27 +0530

    encrypt removed

--- src/main/java/com/fs/sample/PushMessage.java
+++ src/main/java/com/fs/sample/PushMessage.java
@@ -218,98 +218,84 @@ public class PushMessage {
 	    return result;
 	  }
 	
-	public byte[] encrypt(String plainTextString, String uaPublicKeyString,
-		      String authSecret, int paddingSize)
-		      throws InvalidKeyException, NoSuchAlgorithmException, InvalidKeySpecException,
-		      InvalidAlgorithmParameterException, NoSuchPaddingException,
-		      IllegalBlockSizeException, BadPaddingException {
-
-		    // ecdh_secret = ECDH(as_private, ua_public)
-		    // auth_secret = <from user agent>
-		    // salt = random(16)
-		    KeyPair asKeyPair = this.keyPairGenerator.genKeyPair();
-		    ECPublicKey asPublicKey = (ECPublicKey) asKeyPair.getPublic();
-		    byte[] uncompressedASPublicKey = toUncompressedECPublicKey(asPublicKey);
-
-		    ECPublicKey uaPublicKey = fromUncompressedECPublicKey(uaPublicKeyString);
-
-		    KeyAgreement keyAgreement = KeyAgreement.getInstance("ECDH");
-		    keyAgreement.init(asKeyPair.getPrivate());
-		    keyAgreement.doPhase(uaPublicKey, true);
-
-		    byte[] ecdhSecret = keyAgreement.generateSecret();
-
-		    byte[] salt = new byte[16];
-		    this.SECURE_RANDOM.nextBytes(salt);
-
-		    // ## Use HKDF to combine the ECDH and authentication secrets
-		    // # HKDF-Extract(salt=auth_secret, IKM=ecdh_secret)
-		    // PRK_key = HMAC-SHA-256(auth_secret, ecdh_secret)
-		    Mac hmacSHA256 = Mac.getInstance("HmacSHA256");
-		    hmacSHA256
-		        .init(new SecretKeySpec(Base64.getUrlDecoder().decode(authSecret), "HmacSHA256"));
-		    byte[] prkKey = hmacSHA256.doFinal(ecdhSecret);
-
-		    // # HKDF-Expand(PRK_key, key_info, L_key=32)
-		    // key_info = "WebPush: info" || 0x00 || ua_public || as_public
-
-		    byte[] keyInfo = concat("WebPush: info\0".getBytes(StandardCharsets.UTF_8),
-		        toUncompressedECPublicKey(uaPublicKey), uncompressedASPublicKey);
-		    // IKM = HMAC-SHA-256(PRK_key, key_info || 0x01)
-		    hmacSHA256.init(new SecretKeySpec(prkKey, "HmacSHA256"));
-		    hmacSHA256.update(keyInfo);
-		    hmacSHA256.update((byte) 1);
-		    byte[] ikm = hmacSHA256.doFinal();
-
-		    // ## HKDF calculations from RFC 8188
-		    // # HKDF-Extract(salt, IKM)
-		    // PRK = HMAC-SHA-256(salt, IKM)
-		    hmacSHA256.init(new SecretKeySpec(salt, "HmacSHA256"));
-		    byte[] prk = hmacSHA256.doFinal(ikm);
-
-		    // # HKDF-Expand(PRK, cek_info, L_cek=16)
-		    // cek_info = "Content-Encoding: aes128gcm" || 0x00
-		    byte[] cekInfo = "Content-Encoding: aes128gcm\0".getBytes(StandardCharsets.UTF_8);
-		    // CEK = HMAC-SHA-256(PRK, cek_info || 0x01)[0..15]
-		    hmacSHA256.init(new SecretKeySpec(prk, "HmacSHA256"));
-		    hmacSHA256.update(cekInfo);
-		    hmacSHA256.update((byte) 1);
-		    byte[] cek = hmacSHA256.doFinal();
-		    cek = Arrays.copyOfRange(cek, 0, 16);
-
-		    // # HKDF-Expand(PRK, nonce_info, L_nonce=12)
-		    // nonce_info = "Content-Encoding: nonce" || 0x00
-		    byte[] nonceInfo = "Content-Encoding: nonce\0".getBytes(StandardCharsets.UTF_8);
-		    // NONCE = HMAC-SHA-256(PRK, nonce_info || 0x01)[0..11]
-		    hmacSHA256.init(new SecretKeySpec(prk, "HmacSHA256"));
-		    hmacSHA256.update(nonceInfo);
-		    hmacSHA256.update((byte) 1);
-		    byte[] nonce = hmacSHA256.doFinal();
-		    nonce = Arrays.copyOfRange(nonce, 0, 12);
-
-		    Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
-		    cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(cek, "AES"),
-		        new GCMParameterSpec(128, nonce));
-
-		    List<byte[]> inputs = new ArrayList<>();
-		    byte[] plainTextBytes = plainTextString.getBytes(StandardCharsets.UTF_8);
-		    inputs.add(plainTextBytes);
-		    inputs.add(new byte[] { 2 }); // padding delimiter
-
-		    int padSize = Math.max(0, paddingSize - plainTextBytes.length);
-		    if (padSize > 0) {
-		      inputs.add(new byte[padSize]);
-		    }
-
-		    byte[] encrypted = cipher.doFinal(concat(inputs.toArray(new byte[0][])));
-
-		    ByteBuffer encryptedArrayLength = ByteBuffer.allocate(4);
-		    encryptedArrayLength.putInt(encrypted.length);
-
-		    byte[] header = concat(salt, encryptedArrayLength.array(),
-		        new byte[] { (byte) uncompressedASPublicKey.length }, uncompressedASPublicKey);
-
-		    return concat(header, encrypted);
-		  }
+		/*
+		 * public byte[] encrypt(String plainTextString, String uaPublicKeyString,
+		 * String authSecret, int paddingSize) throws InvalidKeyException,
+		 * NoSuchAlgorithmException, InvalidKeySpecException,
+		 * InvalidAlgorithmParameterException, NoSuchPaddingException,
+		 * IllegalBlockSizeException, BadPaddingException {
+		 * 
+		 * // ecdh_secret = ECDH(as_private, ua_public) // auth_secret = <from user
+		 * agent> // salt = random(16) KeyPair asKeyPair =
+		 * this.keyPairGenerator.genKeyPair(); ECPublicKey asPublicKey = (ECPublicKey)
+		 * asKeyPair.getPublic(); byte[] uncompressedASPublicKey =
+		 * toUncompressedECPublicKey(asPublicKey);
+		 * 
+		 * ECPublicKey uaPublicKey = fromUncompressedECPublicKey(uaPublicKeyString);
+		 * 
+		 * KeyAgreement keyAgreement = KeyAgreement.getInstance("ECDH");
+		 * keyAgreement.init(asKeyPair.getPrivate()); keyAgreement.doPhase(uaPublicKey,
+		 * true);
+		 * 
+		 * byte[] ecdhSecret = keyAgreement.generateSecret();
+		 * 
+		 * byte[] salt = new byte[16]; this.SECURE_RANDOM.nextBytes(salt);
+		 * 
+		 * // ## Use HKDF to combine the ECDH and authentication secrets // #
+		 * HKDF-Extract(salt=auth_secret, IKM=ecdh_secret) // PRK_key =
+		 * HMAC-SHA-256(auth_secret, ecdh_secret) Mac hmacSHA256 =
+		 * Mac.getInstance("HmacSHA256"); hmacSHA256 .init(new
+		 * SecretKeySpec(Base64.getUrlDecoder().decode(authSecret), "HmacSHA256"));
+		 * byte[] prkKey = hmacSHA256.doFinal(ecdhSecret);
+		 * 
+		 * // # HKDF-Expand(PRK_key, key_info, L_key=32) // key_info = "WebPush: info"
+		 * || 0x00 || ua_public || as_public
+		 * 
+		 * byte[] keyInfo = concat("WebPush: info\0".getBytes(StandardCharsets.UTF_8),
+		 * toUncompressedECPublicKey(uaPublicKey), uncompressedASPublicKey); // IKM =
+		 * HMAC-SHA-256(PRK_key, key_info || 0x01) hmacSHA256.init(new
+		 * SecretKeySpec(prkKey, "HmacSHA256")); hmacSHA256.update(keyInfo);
+		 * hmacSHA256.update((byte) 1); byte[] ikm = hmacSHA256.doFinal();
+		 * 
+		 * // ## HKDF calculations from RFC 8188 // # HKDF-Extract(salt, IKM) // PRK =
+		 * HMAC-SHA-256(salt, IKM) hmacSHA256.init(new SecretKeySpec(salt,
+		 * "HmacSHA256")); byte[] prk = hmacSHA256.doFinal(ikm);
+		 * 
+		 * // # HKDF-Expand(PRK, cek_info, L_cek=16) // cek_info =
+		 * "Content-Encoding: aes128gcm" || 0x00 byte[] cekInfo =
+		 * "Content-Encoding: aes128gcm\0".getBytes(StandardCharsets.UTF_8); // CEK =
+		 * HMAC-SHA-256(PRK, cek_info || 0x01)[0..15] hmacSHA256.init(new
+		 * SecretKeySpec(prk, "HmacSHA256")); hmacSHA256.update(cekInfo);
+		 * hmacSHA256.update((byte) 1); byte[] cek = hmacSHA256.doFinal(); cek =
+		 * Arrays.copyOfRange(cek, 0, 16);
+		 * 
+		 * // # HKDF-Expand(PRK, nonce_info, L_nonce=12) // nonce_info =
+		 * "Content-Encoding: nonce" || 0x00 byte[] nonceInfo =
+		 * "Content-Encoding: nonce\0".getBytes(StandardCharsets.UTF_8); // NONCE =
+		 * HMAC-SHA-256(PRK, nonce_info || 0x01)[0..11] hmacSHA256.init(new
+		 * SecretKeySpec(prk, "HmacSHA256")); hmacSHA256.update(nonceInfo);
+		 * hmacSHA256.update((byte) 1); byte[] nonce = hmacSHA256.doFinal(); nonce =
+		 * Arrays.copyOfRange(nonce, 0, 12);
+		 * 
+		 * Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+		 * cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(cek, "AES"), new
+		 * GCMParameterSpec(128, nonce)); List<byte[]> inputs = new ArrayList<>();
+		 * byte[] plainTextBytes = plainTextString.getBytes(StandardCharsets.UTF_8);
+		 * inputs.add(plainTextBytes); inputs.add(new byte[] { 2 }); // padding
+		 * delimiter
+		 * 
+		 * int padSize = Math.max(0, paddingSize - plainTextBytes.length); if (padSize >
+		 * 0) { inputs.add(new byte[padSize]); }
+		 * 
+		 * byte[] encrypted = cipher.doFinal(concat(inputs.toArray(new byte[0][])));
+		 * 
+		 * ByteBuffer encryptedArrayLength = ByteBuffer.allocate(4);
+		 * encryptedArrayLength.putInt(encrypted.length);
+		 * 
+		 * byte[] header = concat(salt, encryptedArrayLength.array(), new byte[] {
+		 * (byte) uncompressedASPublicKey.length }, uncompressedASPublicKey);
+		 * 
+		 * return concat(header, encrypted); }
+		 */
 	
 }
